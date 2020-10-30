b2edfc9e9ee74aa9e56c4ff9e5099612f0eb0eb6
ting <dting@thoughtworks.com>
2020-10-30 11:37:02 +1100

    Update mailer sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'spinifexeducation@westnet.com.au'
+  config.mailer_sender = 'gooniyandiapp@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
